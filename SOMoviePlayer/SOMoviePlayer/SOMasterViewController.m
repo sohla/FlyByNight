33f63e371c4c3d430ee9be99d7ac3480be1f0f98
soh_la <soh_la@yahoo.com>
2014-02-17 09:42:37 +1100

    find all movie files and populate table

--- SOMoviePlayer/SOMoviePlayer/SOMasterViewController.m
+++ SOMoviePlayer/SOMoviePlayer/SOMasterViewController.m
@@ -10,9 +10,11 @@
 
 #import "SODetailViewController.h"
 
-@interface SOMasterViewController () {
-    NSMutableArray *_objects;
-}
+@interface SOMasterViewController ()
+    
+@property (retain, nonatomic) NSArray *movieFilePaths;
+    
+
 @end
 
 @implementation SOMasterViewController
@@ -26,10 +28,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    
+    self.movieFilePaths = [self getAllBundleFilesForTypes:@[@"m4v"]];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,14 +39,29 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
+
+-(NSArray*)getAllBundleFilesForTypes:(NSArray*)types{
+    
+    NSError *err = nil;
+    
+    NSMutableArray *collect = [[NSMutableArray alloc] init] ;
+    NSArray *rc = [[NSFileManager defaultManager]
+                   contentsOfDirectoryAtPath:[[NSBundle mainBundle] bundlePath] error:&err];
+    
+    if(err){
+        NSLog(@"%@ : %@ : %@",
+              [err localizedFailureReason],
+              [err localizedDescription],
+              [err localizedRecoverySuggestion]);
     }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    // get all files with correct extensions
+    for(NSString *title in [rc pathsMatchingExtensions:types]){
+        [collect addObject:[[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:title]];
+    }
+    
+    return collect;
+    
 }
 
 #pragma mark - Table View
@@ -57,34 +73,17 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _objects.count;
+    return self.movieFilePaths.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    NSString *title = [[self.movieFilePaths[indexPath.row] lastPathComponent] stringByDeletingPathExtension];
+    cell.textLabel.text = title;
     return cell;
 }
-
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
-
 /*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
@@ -104,9 +103,9 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
+//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+//        NSDate *object = _objects[indexPath.row];
+//        [[segue destinationViewController] setDetailItem:object];
     }
 }
 
