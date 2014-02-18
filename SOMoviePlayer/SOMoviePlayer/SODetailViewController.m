e09227e131ee24ecf725097a483cc39311871d09
soh_la <soh_la@yahoo.com>
2014-02-18 11:04:38 +1100

    added attitude and playback rate

--- SOMoviePlayer/SOMoviePlayer/SODetailViewController.m
+++ SOMoviePlayer/SOMoviePlayer/SODetailViewController.m
@@ -10,7 +10,12 @@
 
 @interface SODetailViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *attitudeLabel;
 @property (retain,nonatomic) MPMoviePlayerController *player;
+@property (retain, nonatomic) CMMotionManager *motionManager;
+@property (retain, nonatomic) CADisplayLink *motionDisplayLink;
+
+@property double motionLastYaw;
 
 
 @end
@@ -20,6 +25,21 @@
 
 #pragma mark - Managing the detail item
 
+-(void)setupMotionManager{
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.motionManager.deviceMotionUpdateInterval = 0.02;  // 50 Hz
+    
+    self.motionDisplayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(motionRefresh:)];
+    [self.motionDisplayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    if ([self.motionManager isDeviceMotionAvailable]) {
+        // to avoid using more CPU than necessary we use `CMAttitudeReferenceFrameXArbitraryZVertical`
+        [self.motionManager startDeviceMotionUpdatesUsingReferenceFrame:CMAttitudeReferenceFrameXArbitraryZVertical];
+    }
+    
+}
+
 - (void)configureView{
 
     NSURL *url  = [NSURL fileURLWithPath:self.movieFilePath];
@@ -28,7 +48,6 @@
     [self.player setScalingMode:MPMovieScalingModeAspectFit];
     [self.player prepareToPlay];
     [self.player setFullscreen:YES animated:YES];
-
     [self addObservers];
     
     // flip it to landscape ratio
@@ -37,8 +56,10 @@
                                         self.view.bounds.size.width);
     
     [self.view addSubview:self.player.view];
-
+    [self.view sendSubviewToBack:self.player.view];
+    
     [self.player play];
+    [self.player setCurrentPlaybackRate:0.5];
 
     UISwipeGestureRecognizer *swipeGesture = [[UISwipeGestureRecognizer alloc]
                                               initWithTarget:self
@@ -50,6 +71,12 @@
 - (void)onSwipeRight:(UIGestureRecognizer *)gestureRecognizer{
     
     [self.navigationController popViewControllerAnimated:YES];
+    [self removeObservers];
+    
+    [self.motionDisplayLink removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    self.motionDisplayLink = nil;
+    self.motionManager = nil;
+
 }
 -(void)addObservers{
 
@@ -79,11 +106,20 @@
         [self configureView];
     }
 }
+-(void)viewWillAppear:(BOOL)animated{
+    [[self navigationController] setNavigationBarHidden:YES animated:YES];
+}
 
+-(void)viewWillDisappear:(BOOL)animated{
+    [[self navigationController] setNavigationBarHidden:NO animated:YES];
+}
 - (void)viewDidLoad{
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
  
+
+    [self.view setBackgroundColor:[UIColor blackColor]];
+    [self setupMotionManager];
     
 }
 
@@ -91,7 +127,9 @@
 {
     [self removeObservers];
     
-    
+    [self.motionDisplayLink removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    self.motionDisplayLink = nil;
+    self.motionManager = nil;
     [super viewDidUnload];
 }
 
@@ -100,6 +138,62 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark Motion Refresher
+
+- (void)motionRefresh:(id)sender {
+    
+    // found at : http://www.dulaccc.me/2013/03/computing-the-ios-device-tilt.html
+    
+    CMQuaternion quat = self.motionManager.deviceMotion.attitude.quaternion;
+    double yaw = asin(2*(quat.x*quat.z - quat.w*quat.y));
+    
+    if (self.motionLastYaw == 0) {
+        self.motionLastYaw = yaw;
+    }
+    
+    // kalman filtering
+    static float q = 0.1;   // process noise
+    static float r = 0.1;   // sensor noise
+    static float p = 0.1;   // estimated error
+    static float k = 0.5;   // kalman filter gain
+    
+    float x = self.motionLastYaw;
+    p = p + q;
+    k = p / (p + r);
+    x = x + k*(yaw - x);
+    p = (1 - k)*p;
+    self.motionLastYaw = x;
+    
+    float speed = (-x / 1.5) * 2.0;
+    
+    if(speed < 0.1) speed = 0.1;
+    if(speed > 2.0) speed = 2.0;
+
+    self.attitudeLabel.text = [NSString stringWithFormat:@"%.2f",speed];
+    
+    //[self.player setCurrentPlaybackRate:speed];
+
+    float max = 5.0;
+    float yaxis = ((-x / 1.5) - 0.5) * max;
+    
+    if(yaxis < -max) yaxis = -max;
+    if(yaxis > max) yaxis = max;
+    self.player.view.frame = CGRectOffset(self.player.view.frame, 0.0, yaxis);
+    
+    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if(orientation == UIInterfaceOrientationLandscapeLeft){
+        
+        
+    }else if (orientation == UIInterfaceOrientationLandscapeRight){
+        
+        
+    }
+    
+}
+
 #pragma mark Notifications
 
 - (void) moviePlayBackDidFinish:(NSNotification*)notification{
--- SOMoviePlayer/SOMoviePlayer/SOMasterViewController.m
+++ SOMoviePlayer/SOMoviePlayer/SOMasterViewController.m
@@ -30,7 +30,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    [[self navigationController] setNavigationBarHidden:YES animated:YES];
 
     self.movieFilePaths = [self getAllBundleFilesForTypes:@[@"m4v",@"mov"]];
     
