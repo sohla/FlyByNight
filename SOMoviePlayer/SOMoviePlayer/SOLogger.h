ee3191c84a78418ccf868202072b187a05c862c9
soh_la <soh_la@yahoo.com>
2015-04-29 11:17:43 +1000

    added file logger

---
+++ SOMoviePlayer/SOMoviePlayer/SOLogger.h
@@ -0,0 +1,15 @@
+//
+//  SOLogger.h
+//  SOMoviePlayer
+//
+//  Created by Stephen OHara on 29/04/2015.
+//  Copyright (c) 2015 Stephen OHara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOLogger : NSObject
+
++ (void)logWithString:(NSString *)msg;
+
+@end
